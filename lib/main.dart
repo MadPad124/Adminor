270d1484ad885a4a82d57c0a526c17d8f8dcacc6
MadPad124 <sinanajandimanesh@gmail.com>
2023-07-19 20:16:16 +0330

    New

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:page_transition/page_transition.dart';
 // ahoooosaf2
+// sadsafiourw
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations(
